92764fd93730dad94ee849b6f1ff099b8b559b69
Andy Walpole <me@andywalpole.me>
2018-04-22 00:07:38 +0100

    added JavaScript linting

--- src/front-non-library/js/vendor/additional-modernizr.js
+++ src/front-non-library/js/vendor/additional-modernizr.js
@@ -1,11 +1,11 @@
-!(function(Modernizr, document) {
-// IE10
+(function(Modernizr, document) {
+	// IE10
 	Modernizr.addTest('ie10', function() {
 		// http://stackoverflow.com/questions/9900311/how-do-i-target-only-internet-explorer-10-for-certain-situations-like-internet-e
 		return (/*@cc_on!@*/false && document.documentMode === 10);
 	});
 
-// IE11
+	// IE11
 	Modernizr.addTest('ie11', function() {
 		// https://en.wikipedia.org/wiki/Internet_Explorer_11
 		// var ua = navigator.userAgent;
@@ -20,4 +20,4 @@
 		return match && (window.navigator.userAgent.indexOf('Edge/') === -1);
 	});
 
-}(Modernizr, document));
+}(Modernizr, document)); // eslint-disable-line no-undef
