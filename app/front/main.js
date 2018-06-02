ae15748ef0b890c2fafee8e3794461b3d7a93ba4
Andy Walpole <me@andywalpole.me>
2018-06-02 13:11:25 +0100

    added basic nav styling

--- app/front/main.js
+++ app/front/main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Nav from './nav';
+
 class Main extends React.Component {
 
 	render () {
 		return (
-			<div>
+			<div className='wrapper'>
+				<Nav />
 				<h1>Here is the site</h1>
 				<p>This is a paragraph</p>
 			</div>
