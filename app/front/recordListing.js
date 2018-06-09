2bb95c2f90c286f495f53a8bccc2771e98a4af94
Andy Walpole <me@andywalpole.me>
2018-06-09 08:15:56 +0100

    increased default node version to 10.4

--- app/front/recordListing.js
+++ app/front/recordListing.js
@@ -8,7 +8,7 @@ class RecordListing extends React.Component {
 		return (
 			<main styleName='recordListing'>
 				<header className='record'>
-					<h1>Al Campbell: Gone Down the Drain</h1>
+					<h2>Al Campbell: Gone Down the Drain</h2>
 					<p>(Reggae Road Production, 1978)</p>
 				</header>
 				<section>
@@ -17,6 +17,31 @@ class RecordListing extends React.Component {
 						<img src={MAIN} alt='temp pictureee'/>
 						<img src={sine} alt='sine waves'/>
 					</div>
+				</section>
+
+				<section styleName='description'>
+					<p>Al Campbell is one of the few reggae vocalists from the long Jamaican Golden Era whose re-releases I would buy without hesitation. This track, delivered in his usual melodic style, is a curiosity because of differential lyrics.</p>
+
+					<p>Campbell lists Jamaican Black nationalist figures - Marcus Garvey, Paul Bogle and George Gordon - asking ‘Do you remember?’ to which he responds ‘They’ve just gone, gone down the drain. When we remember them, they are sometimes like teardrops in the rain”</p>
+
+					<p>The message is is a bleak inversion of the classic Rastafarian lyrical romanticism.</p>
+
+					<div styleName='categories'>
+						<ul>
+							<li>Category One</li>
+							<li>Category two</li>
+							<li>Category three</li>
+						</ul>
+					</div>
+
+				</section>
+
+				<section styleName='comments-form'>
+					<h2>Comments</h2>
+
+					<form>
+
+					</form>
 
 				</section>
 			</main>
