89b1eeb9df0e29a32191fb687a96f25884cd102a
Andy Walpole <me@andywalpole.me>
2018-06-18 16:35:03 +0100

    added nodemon package

--- node/index.js
+++ node/index.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
 	next(createError(404));
