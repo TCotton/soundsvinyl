c53fd156ce3423e32a44d494b2565d46b14cba9e
Andy Walpole <me@andywalpole.me>
2018-06-25 10:02:17 +0100

    added sass support to helloWorld

--- app/admin/components/HelloWorld.vue
+++ app/admin/components/HelloWorld.vue
@@ -3,82 +3,7 @@
 		<h1>{{ msg }}</h1>
 		<h2>Essential Links</h2>
 		<ul>
-			<li>
-				<a
-					href="https://vuejs.org"
-					target="_blank"
-				>
-					Core Docs
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://forum.vuejs.org"
-					target="_blank"
-				>
-					Forum
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://chat.vuejs.org"
-					target="_blank"
-				>
-					Community Chat
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://twitter.com/vuejs"
-					target="_blank"
-				>
-					Twitter
-				</a>
-			</li>
-			<br>
-			<li>
-				<a
-					href="http://vuejs-templates.github.io/webpack/"
-					target="_blank"
-				>
-					Docs for This Template
-				</a>
-			</li>
-		</ul>
-		<h2>Ecosystem</h2>
-		<ul>
-			<li>
-				<a
-					href="http://router.vuejs.org/"
-					target="_blank"
-				>
-					vue-router
-				</a>
-			</li>
-			<li>
-				<a
-					href="http://vuex.vuejs.org/"
-					target="_blank"
-				>
-					vuex
-				</a>
-			</li>
-			<li>
-				<a
-					href="http://vue-loader.vuejs.org/"
-					target="_blank"
-				>
-					vue-loader
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://github.com/vuejs/awesome-vue"
-					target="_blank"
-				>
-					awesome-vue
-				</a>
-			</li>
+			<li>Here is the list</li>
 		</ul>
 	</div>
 </template>
@@ -90,12 +15,17 @@
 			return {
 				msg: 'Welcome to Your Vue.js App'
 			}
+		},
+		mounted() {
+			console.log('the Hello World is mounted');
 		}
 	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+	@import '../../assets/sass/tools';
+	@import '../../assets/sass/starter';
 	h1, h2 {
 		font-weight: normal;
 	}
