f4698b575c8b478db53cd6d57337cb9c205f631f
Andy Walpole <me@andywalpole.me>
2018-06-29 15:22:30 +0100

    added superagent

--- app/admin/components/Pages.vue
+++ app/admin/components/Pages.vue
@@ -9,9 +9,24 @@
 </template>
 
 <script>
+	import superagent from 'superagent';
+	import superagentJsonapify from 'superagent-jsonapify';
+	superagentJsonapify(superagent);
+	import superagentCache from 'superagent-cache';
+	superagentCache(superagent);
+
+	import { dummy } from '../../helper_constants';
+
 	export default {
 		name: 'Pages',
 		data () {
+
+			const request = superagent.get(dummy.page)
+				.then((response) => {
+					const body = response.body;
+					console.dir(body);
+				});
+
 			return {
 				msg: 'Welcome to Your Pages section',
 				Pages: []
