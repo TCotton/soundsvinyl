4227c99a6bdfda86567d326e2503658afc38b098
Andy Walpole <me@andywalpole.me>
2018-07-06 12:16:29 +0100

    added Constraint validation API to AddUser component

--- app/admin/components/AddUser.vue
+++ app/admin/components/AddUser.vue
@@ -63,31 +63,21 @@
 				msg: 'Welcome to Add User section'
 			}
 		},
-		mounted () {
-			console.log('the add user section is mounted');
-		},
 		methods: {
-			onSubmit: function (event) {
+			onSubmit (event) {
 				event.preventDefault();
-
-				this.errors = [];
-
-				if (!this.name) {
-					this.errors.push('Name required.');
-				}
-				if (!this.email) {
-					this.errors.push('Email required.');
-				} else if (!this.validEmail(this.email)) {
-					this.errors.push('Valid email required.');
-				}
-
-				if (!this.errors.length) {
-					return true;
-				}
+				this.validatePassword();
 			},
-			validEmail: function (email) {
-				const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
-				return re.test(email);
+			validatePassword () {
+
+				const addUserPasswordOne = document.getElementById('addUserPasswordOne');
+				const addUserPasswordTwo = document.getElementById('addUserPasswordTwo');
+
+				if (addUserPasswordOne.value !== addUserPasswordTwo.value) {
+					addUserPasswordTwo.setCustomValidity('Passwords Don\'t Match');
+				} else {
+					addUserPasswordTwo.setCustomValidity('');
+				}
 			}
 		}
 	}
