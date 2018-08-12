7a6acdc197f4d76a4f987c7706db8dd3a36daee4
Andy Walpole <me@andywalpole.me>
2018-08-12 13:18:17 +0100

    changes to categoriesHomepage

--- app/front/categoriesHomepage.js
+++ app/front/categoriesHomepage.js
@@ -50,14 +50,10 @@ class CategoriesHomepage extends React.Component {
 
 			arrayMap = pageList.map((element, index) => {
 
-/*				console.dir(`thumbnails/thumbnail-${element._id}.png`);
-				console.dir(element);*/
 				Object.assign(element, {
 					thumbnailUrl: window.location.protocol + '//' + window.location.hostname + ':8443/' + `thumbnails/thumbnail-${element._id}.png`
 				});
-
-				// const thumbnail = window.location.protocol + '//' + window.location.hostname + ':8443/' + `thumbnails/thumbnail-${element._id}.png`;
-
+				
 				return (
 					<div key={index}>
 						{
