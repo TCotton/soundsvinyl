6a43ebbc9893af53aa195de7f79cc3ad79d36823
Andy Walpole <me@andywalpole.me>
2018-08-12 01:30:44 +0100

    added more to Component AddPage test

--- app/admin/components/spec/AddPage.spec.js
+++ app/admin/components/spec/AddPage.spec.js
@@ -1,6 +1,6 @@
 import AddPage from '../AddPage.vue';
 
-describe('Component AddPage', () => {
+fdescribe('Component AddPage', () => {
 
 	let component;
 
@@ -15,7 +15,7 @@ describe('Component AddPage', () => {
 	// Inspect the raw component options
 	it('has a created a method object', () => {
 		expect(component.methods).toEqual(jasmine.any(Object));
-	})
+	});
 
 	// Evaluate the results of functions in
 	// the raw component options
@@ -30,5 +30,33 @@ describe('Component AddPage', () => {
 		expect(defaultData.AddPageForm.descriptionTwo).toEqual(jasmine.any(String));
 		expect(defaultData.AddPageForm.descriptionThree).toEqual(jasmine.any(String));
 		expect(defaultData.AddPageForm.categories).toEqual(jasmine.any(String));
-	})
+		expect(defaultData.actionURL).toEqual(jasmine.any(String));
+		expect(defaultData.errorMsg).toEqual(null);
+	});
+
+	describe('validateBeforeSubmit method', () => {
+
+		const $validator = {};
+		const response = {
+			then: (thenFn) => {
+				thenFn(true);
+			}
+		}
+		$validator.validateAll = jasmine.createSpy('validateAll').and.returnValue(response);
+
+		beforeEach(() => {
+			component = AddPage;
+		});
+
+		it('sets correct component types', () => {
+			expect(component).toBeDefined();
+			expect(typeof component.methods).toBe('object');
+			expect(typeof component.methods.validateBeforeSubmit).toBe('function');
+		});
+
+		it('sets correct component types', () => {
+			// Object.assign(component, $validator);
+			component.methods.validateBeforeSubmit();
+		});
+	});
 });
