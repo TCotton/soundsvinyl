284e75dd4152e1af84a1301daf5e9f6b53ba1c00
Andy Walpole <me@andywalpole.me>
2018-08-16 14:56:25 +0100

    new npm

--- app/misc.js
+++ app/misc.js
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { Helmet, HelmetData } from "react-helmet";
+import * as React from 'react';
+import { Helmet, HelmetData } from 'react-helmet';
 
 const Application = () =>
 	<div className="application">
@@ -9,17 +9,5 @@ const Application = () =>
 			<title>My Title</title>
 			<link rel="canonical" href="http://mysite.com/example"/>
 		</Helmet>
-
-		<Helmet>
-			<title>My Title</title>
-			<meta name="description" content="Helmet application"/>
-		</Helmet>
-
-		<div>
-			<Helmet>
-				<title>Nested Title</title>
-				<meta name="description" content="Nested component"/>
-			</Helmet>
-		</div>
 
 	</div>;
