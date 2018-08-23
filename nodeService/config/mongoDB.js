9c05e472364f53127190503524abd58f0030db11
Andy Walpole <me@andywalpole.me>
2018-08-23 21:26:05 +0100

    changed itemes ready for release on branch 1.1.1

---
+++ nodeService/config/mongoDB.js
@@ -0,0 +1,4 @@
+module.exports = {
+	productionURI : 'mongodb://DCotton:ZQaPtBAjCUgQLPvtWZUEnR2QYDKtER@ds139231-a0.mlab.com:39231,ds139231-a1.mlab.com:39231/soundsvinyl?replicaSet=rs-ds139231',
+	developmentURI : 'localhost:27017/soundsvinyl'
+};
