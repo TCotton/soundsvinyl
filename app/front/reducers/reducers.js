87265d1feabb716dfdb3d6ae20d314fec566b143
Andy Walpole <me@andywalpole.me>
2018-09-02 18:10:37 +0100

    added test for searchTextReducer

--- app/front/reducers/reducers.js
+++ app/front/reducers/reducers.js
@@ -1,109 +1,12 @@
-// import React from 'react';
-/*import { combineReducers, createStore, compose } from 'redux';
-import axios from 'axios';*/
-
-export const searchReducer = (state = '', action) => {
+export const searchTextReducer = (state = '', action) => {
 	switch (action.type) {
-		case 'CHANGE_SEARCH':
-			return action.name;
+		case 'SET_SEARCH_TEXT':
+			alert(action);
+			alert(action.searchTerm);
+			alert(action.type);
+			return action.searchTerm;
 		default:
+			alert(state);
 			return state;
 	}
 }
-
-/*
-const nameReducer = (state = 'Anonymous', action) => {
-	switch (action.type) {
-		case 'CHANGE_NAME':
-			return action.name;
-		default:
-			return state;
-	}
-}
-
-let nextHobbyId = 1;
-
-const hobbiesReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_HOBBY':
-			return [
-				...state,
-				{
-					id: nextHobbyId++,
-					hobby: action.hobbyd
-				}
-			]
-		case 'REMOVE_HOBBY':
-			return state.filter((hobby) => hobby.id !== action.id)
-		default:
-			return state;
-	}
-}
-
-const mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-	switch (action.type) {
-		case 'STATE_LOCATION_FETCH':
-			return {
-				isFetching: true,
-				url: undefined,
-			}
-		case 'COMPLETE_LOCATION_FETCH':
-			return {
-				isFetching: false,
-				url: action.url
-			}
-		default:
-			return state;
-	}
-};
-
-const startLocationFetch = () => {
-	return {
-		type: 'START_LOCATION'
-	}
-}
-
-const completeLocationFetch = (url) => {
-	return {
-		type: 'COMPLETE_LOCATION_FETCH',
-		url,
-	}
-}
-
-const fetchLocation = () => {
-	store.dispatch(startLocationFetch());
-
-	axios.get('http://ipinfo.io').then((res) => {
-		const loc = res.data.loc;
-		const baseUrl = 'http://maps.google.com?q='
-
-		store.dispatch(completeLocationFetch(baseUrl + loc));
-	});
-}
-
-fetchLocation();
-
-const reducer = combineReducers({
-	name: nameReducer,
-	hobby: hobbiesReducer,
-	map: mapReducer,
-});
-
-const store = createStore(reducer, compose(
-	window.devToolsExtenion ? window.devToolsExtenion : f => f
-));
-
-const unsubscribe = store.subscribe(() => {
-	const state = store.getState();
-
-	console.log('Name is ', state.name);
-	document.getElementById('app').innerHTML = state.name;
-	console.log('New state  ', store.getState());
-
-	if(state.map.isFetching) {
-		document.getElementById('app').innerHTML = 'Loading...';
-	} else {
-		document.getElementById('app').innerHTML = '<a href="' + state.map.url  + '" target="_blank">View your location</a>';
-	}
-});
-*/
---
+++ app/front/reducers/spec/reducers.spec.js
@@ -0,0 +1,54 @@
+import { searchTextReducer } from '../reducers';
+
+describe('Reducers', () => {
+
+	let reducer;
+
+	let action = {}
+
+	xdescribe('searchTextReducer - correct switch statement', () => {
+
+		beforeEach(() => {
+
+			let action = {
+				searchTerm: 'This is a search term',
+				type: 'SET_SEARCH_TEXT',
+			}
+
+			reducer = searchTextReducer('', action);
+		});
+
+		it('should be defined', () => {
+			expect(reducer).toBeDefined();
+		});
+
+		it(`should have same value as ${action.searchTerm}`, () => {
+			expect(reducer).toEqual(action.searchTerm);
+			expect(reducer).toEqual(jasmine.any(String));
+			expect(reducer.length).toEqual(action.searchTerm.length);
+		});
+	});
+
+	describe('searchTextReducer - default switch statement', () => {
+
+		beforeEach(() => {
+
+			let action = {
+				searchTerm: 'This is a search term',
+				type: 'NOT_SET_SEARCH_TEXT',
+			}
+
+			reducer = searchTextReducer('', action);
+		});
+
+		it('should be defined', () => {
+			expect(reducer).toBeDefined();
+		});
+
+		it(`should have empty string as default`, () => {
+			expect(reducer).toEqual('');
+			expect(reducer).toEqual(jasmine.any(String));
+			expect(reducer.length).toEqual(0);
+		});
+	});
+});
