58c406828ac9a8ef22fba2ef55da93ff8f341945
Andy Walpole <me@andywalpole.me>
2018-11-09 17:27:31 +0000

    removed all trace of spa for react frontend

---
+++ app/front/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import './misc/oauth';
+
+import '../assets/sass/index.scss';
+import Wrapper from './Wrapper';
+
+/*import { setSearchText } from './actions/actions';
+import { store } from './store/configureStore';
+
+store.subscribe(() => {
+	console.log('new store', store.getState());
+});
+
+store.dispatch(setSearchText('This is a new search term'));*/
+
+export default class App extends Component {
+	render () {
+		return (
+			<CookiesProvider>
+				<Router>
+					<Wrapper />
+				</Router>
+			</CookiesProvider>
+		);
+	}
+}
--- app/front/index.js
+++ app/front/index.js
@@ -1,28 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
-import './misc/oauth';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import '../assets/sass/index.scss';
-import Wrapper from './Wrapper';
+const RenderedApp = () => (
+	<App />
+);
 
-/*import { setSearchText } from './actions/actions';
-import { store } from './store/configureStore';
-
-store.subscribe(() => {
-	console.log('new store', store.getState());
-});
-
-store.dispatch(setSearchText('This is a new search term'));*/
-
-export default class rootComponent extends Component {
-	render () {
-		return (
-			<CookiesProvider>
-				<Router>
-					<Wrapper />
-				</Router>
-			</CookiesProvider>
-		);
-	}
-}
+ReactDOM.render( <RenderedApp />, document.getElementById( 'root' ) );
