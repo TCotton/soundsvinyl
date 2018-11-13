dfe6a98aa552685cb5a78fd7181e3061c3ecfc55
Andy Walpole <me@andywalpole.me>
2018-11-13 18:46:24 +0000

    added a CommentsPolicy component

--- app/front/Comments/CommentsPolicy.js
+++ app/front/Comments/CommentsPolicy.js
@@ -15,22 +15,25 @@ export default class CommentsPolicy extends Component {
 					{'Comment policy'}
 				</h2>
 				<p>
-					{'In the spring of 1976, Eric Clapton, Britain’s leading Blues proponent, gav a speech on a gig platform so racist it ignited mass-scale anti-racist movements, the Anti-Nazi League (ANL) and Rock and Racism (RAR).'}
+					{'In the spring of 1976, Eric Clapton, Britain’s leading Blues guitarist of ten-years-standing, gave a speech from the platform at a gig in Birminghamd that was so racist it ignited mass-scale anti-racist movements, the Anti-Nazi League (ANL) and Rock Against Racism (RAR).'}
 				</p>
 				<p>
-					{'His speech was violence without punches: "Vote for Enoch Powell... I think Enoch\'s right, I think we should send them all back. Stop Britain from becoming a black colony. Get the foreigners out. Get the wogs out. Get the coons out. Keep Britain white.".'}
+					{'His speech was verbal violent assault, rather than physical: "Vote for Enoch Powell... I think Enoch\'s right, I think we should send them all back. Stop Britain from becoming a black colony. Get the foreigners out. Get the wogs out. Get the coons out. Keep Britain white.".'}
 				</p>
 				<p>
-					{'To this day, he has never satisfactorily his actions at that gig in Birmingham'}
+					{'To this day, he has never satisfactorily his actions at that infamous gig in Birmingham'}
 				</p>
 				<p>
 					{' Black music, for him, was acceptable to financially exploit, but stopped at inviting Black musicians to be neighbours on the same street.'}
 				</p>
 				<p>
-					{' The language of xenophobia, revolting now as then, is not welcome on the scene, and that includes this site, SoundsVinyl.'}
+					{' The language of xenophobia, revolting now as then, is still not welcome on the scene, and that includes this site, SoundsVinyl.'}
 				</p>
 				<p>
-					{' No racism, sexism, homophobia or any form of xenophobia will be tolerated in the comments section of this site. No ifs, no buts, kick out it out.'}
+					{' No racism, sexism, homophobia or any form of xenophobia will be tolerated in the comments section of this site.'}
+				</p>
+				<p>
+					{'No ifs, no buts, just kick out it out'}
 				</p>
 			</section>
 		);
--- app/front/Disqus/Disqus.js
+++ app/front/Disqus/Disqus.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Disqus from 'disqus-react';
 import './disqus.scss';
+
 class Disqus extends Component {
-	render () {
+
+	render() {
+		const disqusShortname = 'example';
+		const disqusConfig = {
+			url: this.props.article.url,
+			identifier: this.props.article.id,
+			title: this.props.article.title,
+		};
 
 		return (
-			<React.Fragment />
-		)
+			<div className="article">
+				<h1>{this.props.article.title}</h1>
+				<Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
+					Comments
+				</Disqus.CommentCount>
+				<p>{this.props.article.body}</p>
+				<Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+			</div>
+		);
 	}
 }
 
