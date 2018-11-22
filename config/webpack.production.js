8080c465fa135eee4ebdf7a315f4295872968684
Andy Walpole <me@andywalpole.me>
2018-11-22 21:56:24 +0000

    added image-webpack-loader to help with image optimisation

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -1,10 +1,10 @@
-const webpack = require('webpack');
-const path = require('path');
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.common.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const SizePlugin = require('size-plugin');
+const webpack = require( 'webpack' );
+const path = require( 'path' );
+const webpackMerge = require( 'webpack-merge' );
+const commonConfig = require( './webpack.common.js' );
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const SizePlugin = require( 'size-plugin' );
 // const ImageminPlugin = require('imagemin-webpack-plugin');
 
 // the path(s) that should be cleaned
@@ -12,7 +12,7 @@ const pathsToClean = [
 	global.__base + '/dist',
 ]
 
-module.exports = webpackMerge(commonConfig, {
+module.exports = webpackMerge( commonConfig, {
 	mode: 'production',
 	stats: {
 		colors: true,
@@ -24,38 +24,108 @@ module.exports = webpackMerge(commonConfig, {
 		modules: true,
 		children: true,
 	},
+	module: {
+		rules: [
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				exclude: ( /node_modules/ ),
+				use: [
+					{
+						loader: 'file-loader'
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: '65-90',
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75
+							}
+						}
+					}
+				]
+			}
+		],
+	},
 	devtool: 'hidden-source-map',
 	output: {
 		filename: '[name].[contenthash].js',
 		chunkFilename: '[name].[contenthash].chunk.js',
-		path: path.resolve(global.__base, 'dist')
+		path: path.resolve( global.__base, 'dist' )
 	},
-/*	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all'
+	/*	optimization: {
+			runtimeChunk: 'single',
+			splitChunks: {
+				cacheGroups: {
+					vendor: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendors',
+						chunks: 'all'
+					}
 				}
 			}
-		}
-	},*/
+		},*/
+
+	/**
+	 * {
+		test: /\.(gif|png|jpe?g|svg)$/i,
+		use: [
+			{
+				loader: 'file-loader'
+			},
+			{
+				loader: 'image-webpack-loader',
+				options: {
+					mozjpeg: {
+						progressive: true,
+						quality: 65
+					},
+					// optipng.enabled: false will disable optipng
+					optipng: {
+						enabled: false,
+					},
+					pngquant: {
+						quality: '65-90',
+						speed: 4
+					},
+					gifsicle: {
+						interlaced: false,
+					},
+					// the webp option will enable WEBP
+					webp: {
+						quality: 75
+					}
+				}
+			},
+	 */
 
 	plugins: [
-		new CleanWebpackPlugin(pathsToClean),
+		new CleanWebpackPlugin( pathsToClean ),
 
-		new webpack.DefinePlugin({
+		new webpack.DefinePlugin( {
 			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
+				'NODE_ENV': JSON.stringify( 'production' )
 			}
-		}),
+		} ),
 
 		// new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
 
 		// /Applications/MAMP/htdocs/soundsvinyl/app/assets/images
-		new CopyWebpackPlugin([
+		new CopyWebpackPlugin( [
 			{
 				from: global.__base + '/src/faviconImages',
 				to: global.__base + '/dist/faviconImages'
@@ -76,7 +146,7 @@ module.exports = webpackMerge(commonConfig, {
 				from: global.__base + '/app/assets/images',
 				to: global.__base + '/dist/app/assets/images'
 			}
-		]),
+		] ),
 
 		new SizePlugin(),
 
@@ -90,4 +160,4 @@ module.exports = webpackMerge(commonConfig, {
 			new webpack.optimize.UglifyJsPlugin(), //minify everything
 			new webpack.optimize.AggressiveMergingPlugin()//Merge chunks*/
 	]
-});
+} );
