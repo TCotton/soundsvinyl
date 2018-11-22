cddb940e2ca173175ca6f7c0696e461c85de2f49
Andy Walpole <me@andywalpole.me>
2018-11-22 22:28:23 +0000

    added first lazy wrapper to test out code

--- app/front/Wrapper.js
+++ app/front/Wrapper.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import Nav from './Navigation/Nav';
 import { Route, Switch } from 'react-router-dom';
 import Main from './main';
-import RecordListing from './recordListing';
 import Contact from './Contact/Contact';
 import AboutUs from './about';
 import MyAccount from './Account/MyAccount';
@@ -13,6 +12,8 @@ import Footer from './Footer/Footer';
 import ErrorBoundary from './errorBoundaries/ErrorBoundary';
 import CommentsPolicy from './Comments/CommentsPolicy';
 
+const RecordListing = lazy( () => import('./recordListing') );
+
 export default class Wrapper extends Component {
 
 	render () {
@@ -21,57 +22,64 @@ export default class Wrapper extends Component {
 				<ErrorBoundary>
 					<Nav />
 				</ErrorBoundary>
-				<Switch>
-					<Route
-						component={Main}
-						exact
-						path='/'
-					/>
-					<Route
-						component={Main}
-						exact
-						path='/category/:tag'
-					/>
-					<Route
-						component={RecordListing}
-						exact
-						path='/:slug/:id'
-					/>
-					<Route
-						component={Contact}
-						exact
-						path='/contact'
-					/>
-					<Route
-						component={AboutUs}
-						exact
-						path='/about-us'
-					/>
-					<Route
-						component={MyAccount}
-						exact
-						path='/my-account'
-					/>
-					<Route
-						component={VinylCharts}
-						exact
-						path='/vinyl-singles-top-30-chart'
-					/>
-					<Route
-						component={CommentsPolicy}
-						exact
-						path='/comments-policy'
-					/>
-					<Route
-						component={Sitemap}
-						exact
-						path='/sitemap'
-					/>
-					<Route
-						component={Error}
-						path=''
-					/>
-				</Switch>
+				<Suspense fallback={
+					<div>
+						{'Loading...'}
+					</div>
+				}
+				>
+					<Switch>
+						<Route
+							component={Main}
+							exact
+							path='/'
+						/>
+						<Route
+							component={Main}
+							exact
+							path='/category/:tag'
+						/>
+						<Route
+							component={RecordListing}
+							exact
+							path='/:slug/:id'
+						/>
+						<Route
+							component={Contact}
+							exact
+							path='/contact'
+						/>
+						<Route
+							component={AboutUs}
+							exact
+							path='/about-us'
+						/>
+						<Route
+							component={MyAccount}
+							exact
+							path='/my-account'
+						/>
+						<Route
+							component={VinylCharts}
+							exact
+							path='/vinyl-singles-top-30-chart'
+						/>
+						<Route
+							component={CommentsPolicy}
+							exact
+							path='/comments-policy'
+						/>
+						<Route
+							component={Sitemap}
+							exact
+							path='/sitemap'
+						/>
+						<Route
+							component={Error}
+							path=''
+						/>
+					</Switch>
+				</Suspense>
 				<ErrorBoundary>
 					<Footer />
 				</ErrorBoundary>
