9ad318a4584c8c3dfbdbabf068aaebe6924d1822
Andy Walpole <me@andywalpole.me>
2019-10-12 14:54:48 +0100

    added rss feed

--- nodeService/routes/rss.js
+++ nodeService/routes/rss.js
@@ -1,7 +1,7 @@
 import { Feed } from 'feed';
 
 module.exports = ( app ) => {
-	
+
 	const feed = new Feed({
 		title: 'vinylsounds: Vinyl forever, music for all',
 		description: 'music review site',
@@ -15,6 +15,54 @@ module.exports = ( app ) => {
 		}
 	});
 
-
-
 }
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+	constructor ( props ) {
+		super( props );
+	}
+
+	componentWillMount () {
+
+	}
+
+	componentDidMount () {
+
+	}
+
+	componentWillReceiveProps ( nextProps ) {
+
+	}
+
+	shouldComponentUpdate ( nextProps, nextState ) {
+
+	}
+
+	componentWillUpdate ( nextProps, nextState ) {
+
+	}
+
+	componentDidUpdate ( prevProps, prevState ) {
+
+	}
+
+	componentWillUnmount () {
+
+	}
+
+	render () {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+MyComponent.propTypes = {};
+
+export default MyComponent;
+
