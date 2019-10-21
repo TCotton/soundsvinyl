1c1ee92a4b34b241c423639ce1003786d780bbe2
Andy Walpole <me@andywalpole.me>
2019-10-21 12:11:07 +0100

    changed text to variables

--- app/front/about.js
+++ app/front/about.js
@@ -1,61 +1,67 @@
-/* eslint-disable react/jsx-no-literals */
 import React from 'react'
 import './about.scss'
 
 class AboutUs extends React.Component {
 	render () {
-		return (
-			<main styleName='aboutUs'>
-				<h2>
-					`About SoundsVinyl`
-				</h2>
-
-				<section>
-					<p>
-						`SoundsVinyl started as a music recommendation site that
+		const $title = `About SoundsVinyl`;
+		const $firstPara = `SoundsVinyl started as a music recommendation site that
 						concentrated on 7 inch and 12 inch vinyl single releases; but this
-						site isn’t about vinyl, that’s the hook on which to hang it.`
-					</p>
-
-					<p>
-						`This site could be interpreted as an exercise in nostalgia. It was
+						site isn’t about vinyl, that’s the hook on which to hang it.`;
+		const $secondPara = `This site could be interpreted as an exercise in nostalgia. It was
 						never intended to be that. If music doesn’t come up from the
 						streets, it loses its vitality. In fact, a vinyl collectors club has
 						all the cultural buzz only of a stamp collectors club.`
-					</p>
-
-					<p>
-						`SoundsVinyl is a site about music. What underpins our attraction to
+		const $thirdPara = `SoundsVinyl is a site about music. What underpins our attraction to
 						music – perhaps all art – is the emotion and the memories that we
 						attach to it. Music is both a communal and a solitary experience. We
 						can share it together with thousands of others or experience it on
 						our own on repeat until we drain all feeling from what we are
-						playing.`
-					</p>
-
-					<p>
-						`The sounds highlighted on this site may offer you a different,
+						playing.`;
+		const $fourthPara = `The sounds highlighted on this site may offer you a different,
 						brief glimpse of sublimity; an emotional epiphany which popular
-						culture is capable of providing just as much as high art can.`
-					</p>
-
-					<p>
-						`SoundsVinyl may offer you a snippet of a rhythm or a songwriter’s
+						culture is capable of providing just as much as high art can.`;
+		const $paraFive = `SoundsVinyl may offer you a snippet of a rhythm or a songwriter’s
 						verse, which may, in turn, inspire you to seek out more from the
 						same artist. But never aim to emulate. What originated from a
 						particular historical era can’t be copied. What has been, has now
 						died and can’t be revived. Sentimental nostalgia is inherently
 						conservative and counter-revolutionary, while hauntology at the end
 						of the second decade of the 21st century is listless to the point of
-						death. Create something new.`
-					</p>
-
-					<p>
-						`I hope that there may be something on here that inspires you in
+						death. Create something new.`;
+		const $paraSix = `I hope that there may be something on here that inspires you in
 						your endeavours; whether you are a game creator, a writer, or a
 						designer. But if there isn’t, come back and visit again next week or
 						next month, as I regularly update the site with all styles of
-						music.`
+						music.`;
+		return (
+			<main styleName='aboutUs'>
+				<h2>
+					{$title}
+				</h2>
+
+				<section>
+					<p>
+						{$firstPara}
+					</p>
+
+					<p>
+						{$secondPara}
+					</p>
+
+					<p>
+						{$thirdPara}
+					</p>
+
+					<p>
+						{$fourthPara}
+					</p>
+
+					<p>
+						{$paraFive}
+					</p>
+
+					<p>
+						{$paraSix}
 					</p>
 				</section>
 			</main>
